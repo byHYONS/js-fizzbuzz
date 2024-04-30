b40f0b85236f4298086aa937ce0e9686c0929562
byHYONS <byhyons@gmail.com>
2024-04-30 20:52:03 +0200

    [UPD] copleted milestone 1 • Fine Esercizio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 // reset
 'use strict'
 
-console.log("ciao!");
 
+// Passo successivo sarà quello di eseguire una istruzione condizionale e far in modo che vengano rispettate le condizioni richieste dall'esercizio circa i `multipli di 3, 5, 15`:
+for (let i = 1; i<=100; i++){
+// Per prima cosa eseguo una istruzione di iterazione con il `for loop`:
+  if (i % 3 === 0 && i % 5 !== 0){
+    console.log('fizz');
+  } else if (i % 5 === 0 && i % 3 !== 0){
+      console.log('Buzz');
+  } else if (i % 3 === 0 && i % 5 === 0){
+      console.log('FizzBuzz');
+  }  else {
+    console.log(i);
+  }
+}
