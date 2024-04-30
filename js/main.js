8aa45a2a4230ad2320563672ccc4b1da282eb7b2
byHYONS <byhyons@gmail.com>
2024-04-30 21:51:15 +0200

    [UPD] bunus 1 copleted

--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
 // reset
 'use strict'
 
-
 // Passo successivo sarà quello di eseguire una istruzione condizionale e far in modo che vengano rispettate le condizioni richieste dall'esercizio circa i `multipli di 3, 5, 15`:
-for (let i = 1; i<=100; i++){
 // Per prima cosa eseguo una istruzione di iterazione con il `for loop`:
+// dichiaro var per leggere il div sul quale costuire i box
+const generaBox = document.querySelector('.boxes');
+console.log({generaBox});
+
+for (let i = 1; i<=100; i++){
   if (i % 3 === 0 && i % 5 !== 0){
     console.log('fizz');
+    const boxFizz = document.createElement('div');
+    boxFizz.classList.add('box', 'box-yellow')
+    boxFizz.append('Fizz');
+    generaBox.append(boxFizz); 
+
   } else if (i % 5 === 0 && i % 3 !== 0){
       console.log('Buzz');
+      const boxBuzz = document.createElement('div');
+      boxBuzz.classList.add('box', 'box-green')
+      boxBuzz.append('Buzz');
+      generaBox.append(boxBuzz);
+
   } else if (i % 3 === 0 && i % 5 === 0){
       console.log('FizzBuzz');
+      const boxFizzBuzz = document.createElement('div');
+      boxFizzBuzz.classList.add('box', 'box-coral')
+      boxFizzBuzz.append('FizzBuzz');
+      generaBox.append(boxFizzBuzz);
+
   }  else {
-    console.log(i);
+      console.log(i);
+      const boxNumber = document.createElement('div');
+      boxNumber.classList.add('box')
+      boxNumber.append(i);
+      generaBox.append(boxNumber);
   }
 }
